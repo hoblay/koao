0e830e26068acf445869abef9a979dec99abdd29
Winslet Mateus <hoblayrecords@gmail.com>
2024-04-06 12:16:40 +0200

    fixed sign up

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -18,7 +18,7 @@ if(session) router.push('/')
   return (
     <div className="relative flex justify-between">
      
-      <main className="bg-zinc-50 dark:bg-zinc-900 w-full items-center justify-center pl-28 py-10 h-[100vh] max-h-[100vh]">
+      <main className="bg-zinc-50 dark:bg-zinc-900 w-full items-center justify-center pl-28 py-10 h-full">
         <Link href={"/"} className="flex gap-2 items-center mb-8"><LogoIcon width='45' height='33' className="#015F43"/> 
           <span className={` text-xl font-semibold text-zinc-700 dark:text-zinc-50 whitespace-nowrap`}>
             Logotipo
