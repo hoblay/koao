0947bcf1e34fa149526c9032693bfd28a66a50d1
Winslet Mateus <hoblayrecords@gmail.com>
2024-04-09 04:36:07 +0200

    fix

--- src/app/(dashboard)/teacher/(course)/[courseId]/_components/AddImage.tsx
+++ src/app/(dashboard)/teacher/(course)/[courseId]/_components/AddImage.tsx
@@ -45,7 +45,7 @@ function AddImage({imageUrl, edit, courseId}: AddImageProps) {
       })
     )
       setImage(img[0]);
-      getCourse.refetch()
+      
         
       }
       reader.readAsDataURL(files)
@@ -69,7 +69,7 @@ function AddImage({imageUrl, edit, courseId}: AddImageProps) {
       },
       body: file,
     })
-
+    getCourse.refetch()
     
   }
 ,[courseId]) 
