fe0aaa7e72d90f81e9751ed22b0d914db0f445de
Winslet Mateus <hoblayrecords@gmail.com>
2024-04-18 19:10:44 +0200

    update: minor design changes

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -15,17 +15,17 @@ export default async function Home() {
   if (!courses) return null;
   return (
     <div className="px-9 flex flex-col gap-4">
-      <div className="relative  flex-col items-start bg-amber-200 dark:bg-amber-950 dark:bg-grid-small-white/[0.2]  bg-dot-black/[0.2]  rounded-xl max-h-[343px] min-h-[340px]">
+      <div className="relative  flex-col items-start bg-blue-200 dark:bg-blue-950 dark:bg-grid-small-white/[0.2]  bg-dot-black/[0.2]  rounded-xl max-h-[343px] min-h-[340px]">
         <div className="py-16 px-10 space-y-2 max-w-[70%]">
           <h2 className="text-zinc-700 pr-20 dark:text-zinc-100 text-xl md:text-4xl font-bold md:leading-[140%] line-clamp-2">
-            {courses[courses.length - 2].title}
+            {courses[courses.length - 1].title}
           </h2>
           <p className="text-zinc-700 dark:text-zinc-200 text-sm md:text-base line-clamp-2 md:line-clamp-2">
             {courses[courses.length - 1].description}
           </p>
           <div className="flex gap-3 py-2">
             <Tag
-              name={`${courses[courses.length - 2].chapters.length} Modulos`}
+              name={`${courses[courses.length - 1].chapters.length} Modulos`}
               className="bg-zinc-50/70"
               startContent={
                 <IconNotebook className="w-5 h-5 text-zinc-500 dark:text-zinc-400" />
@@ -38,7 +38,7 @@ export default async function Home() {
         <div className="absolute max-w-[345px] right-8 top-8 justify-center items-center pt-4 pb-6 px-4 bg-zinc-50/50 dark:bg-zinc-900/50 w-full  rounded-xl shadow-sm flex-col space-y-4">
           <div className="relative rounded-xl">
             <Image
-              src={courses[courses.length - 2].imageUrl || ""}
+              src={courses[courses.length - 1].imageUrl || ""}
               className="rounded-xl w-[313px] max-h-[176px] object-cover"
               alt="course"
               width={313}
@@ -47,19 +47,19 @@ export default async function Home() {
             />
           </div>
 
-          {courses[courses.length - 2].chapters[0].lessons[0] && (
+          {courses[courses.length - 1].chapters[0].lessons[0] && (
             <div className="px-2">
               <Link
-                href={`/watch/${courses[0].id}/${courses[courses.length - 2].chapters[0].lessons[0].id}`}
+                href={`/watch/${courses[0].id}/${courses[courses.length - 1].chapters[0].lessons[0].id}`}
                 className=""
               >
                 <button
                   type="button"
-                  className="relative inline-flex flex-shrink-0 justify-center items-center rounded-md transition-colors ease-in-out duration-200 font-semibold disabled:opacity-50 disabled:cursor-not-allowed disabled:select-none border-none cursor-pointer bg-zinc-50 hover:bg-amber-500 text-[#143229]  hover:text-white px-8 py-3 text-2xl w-full"
+                  className="relative inline-flex flex-shrink-0 justify-center items-center rounded-md transition-colors ease-in-out duration-200 font-semibold disabled:opacity-50 disabled:cursor-not-allowed disabled:select-none border-none cursor-pointer bg-zinc-50 hover:bg-blue-500 text-[#143229]  hover:text-white px-8 py-3 text-2xl w-full"
                 >
                   <div className="flex flex-1 justify-center items-center gap-2">
                     <span className="text-base leading-6">
-                      Continuar a assistir
+                      Começar a assistir
                     </span>
                     <IconPlayerPlay />
                   </div>
