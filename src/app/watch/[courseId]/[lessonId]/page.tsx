178b47f4613393c9c2d078b7d1f6c32aa0802419
Winslet Mateus <hoblayrecords@gmail.com>
2024-04-18 18:47:01 +0200

    update: Autoplay

--- src/app/watch/[courseId]/[lessonId]/page.tsx
+++ src/app/watch/[courseId]/[lessonId]/page.tsx
@@ -21,6 +21,7 @@ export default function ClassPage({
           width={"100%"}
           className="absolute top-0 left-0"
           height={"100%"}
+          autoplay
           url={lesson.data.video?.commitUrl ? lesson.data.video?.commitUrl : ""}
           config={{}}
         />
