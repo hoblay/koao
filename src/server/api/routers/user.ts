432490d96bd98a48a682447e1b43fff895f8430c
Winslet Mateus <hoblayrecords@gmail.com>
2024-04-24 18:27:14 +0200

    fix: api issues

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,45 +1,47 @@
-import { SignUpSchema, TSignUpSchema } from "@/schemas"
-import { publicProcedure, router } from "@/server/api/trpc"
+import { SignUpSchema, TSignUpSchema } from "@/schemas";
+import { publicProcedure, router } from "@/server/api/trpc";
 import { db } from "@/server/db";
 import { hash } from "bcrypt";
 import { NextResponse } from "next/server";
 
 export const userRouter = router({
+  create: publicProcedure.input(SignUpSchema).mutation(async (opts) => {
+    try {
+      const { input } = opts;
+      const { name, email, password } = input;
+      // Check if email already exists
+      const existingUserByEmail = await db.user.findUnique({
+        where: { email: email },
+      });
 
+      if (existingUserByEmail)
+        return NextResponse.json(
+          { user: null, message: "Este email ja está sendo usado." },
+          { status: 409 },
+        );
 
-  create: publicProcedure
-    .input(SignUpSchema)
-    .mutation(async (opts) => { 
-      try {
-        const { input } = opts;
-        const { name, email, password, dateOfBirth, plan } = input
-        // Check if email already exists
-        const existingUserByEmail = await db.user.findUnique({
-          where: {email: email}
-        })
+      const hashedPassword = await hash(password, 10);
 
-        if (existingUserByEmail) return NextResponse.json({user: null, message: 'Este email ja está sendo usado.'}, {status: 409})
+      const newUser = await db.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+          n,
+        },
+      });
 
+      const { password: newPassword, ...rest } = newUser;
 
-        const hashedPassword = await hash(password, 10)
-
-        const newUser = await db.user.create({
-          data: {
-            name,
-            email,
-            password: hashedPassword,
-            dateOfBirth,
-            plan
-          }
-        })
-
-        const { password: newPassword, ...rest } = newUser;
-
-        return NextResponse.json({ user: rest, message: 'Usuario criado com sucesso.'}, { status: 201})
-      } catch (error) {
-        
-        return NextResponse.json({message: 'Algo errado não está certo', error}, { status: 500 })
-      }
-    }),
-
+      return NextResponse.json(
+        { user: rest, message: "Usuario criado com sucesso." },
+        { status: 201 },
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Algo errado não está certo", error },
+        { status: 500 },
+      );
+    }
+  }),
 });
