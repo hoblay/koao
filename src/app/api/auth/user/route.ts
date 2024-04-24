492305e58b1acf80afdfd915e3a31ce323ef10de
Winslet Mateus <hoblayrecords@gmail.com>
2024-04-24 18:25:38 +0200

    fix: api issues

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,38 +1,44 @@
 import { db } from "@/server/db";
 import { NextResponse } from "next/server";
-import { hash } from 'bcrypt';
+import { hash } from "bcrypt";
 import { SignUpSchema } from "@/schemas";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { name, email, password, dateOfBirth, plan } = SignUpSchema.parse(body)
+    const { name, email, password } = SignUpSchema.parse(body);
 
     // Check if email already exists
     const existingUserByEmail = await db.user.findUnique({
-      where: {email: email}
-    })
+      where: { email: email },
+    });
 
-    if (existingUserByEmail) return NextResponse.json({user: null, message: 'Este email ja está sendo usado.'}, {status: 409})
+    if (existingUserByEmail)
+      return NextResponse.json(
+        { user: null, message: "Este email ja está sendo usado." },
+        { status: 409 },
+      );
 
-
-    const hashedPassword = await hash(password, 10)
+    const hashedPassword = await hash(password, 10);
 
     const newUser = await db.user.create({
       data: {
         name,
         email,
         password: hashedPassword,
-        dateOfBirth,
-        plan
-      }
-    })
+      },
+    });
 
     const { password: newPassword, ...rest } = newUser;
 
-    return NextResponse.json({ user: rest, message: 'Usuario criado com sucesso.'}, { status: 201})
-
+    return NextResponse.json(
+      { user: rest, message: "Usuario criado com sucesso." },
+      { status: 201 },
+    );
   } catch (error) {
-    return NextResponse.json({message: 'Algo errado não está certo', error}, { status: 500 })
+    return NextResponse.json(
+      { message: "Algo errado não está certo", error },
+      { status: 500 },
+    );
   }
 }
