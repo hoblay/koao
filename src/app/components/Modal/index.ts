b00315b6ce8c2d307e4460fb664de6d010c86ac9
Winslet Mateus <hoblayrecords@gmail.com>
2024-05-07 12:01:19 +0200

    fix: deleted modal trigger part 2

--- src/app/components/Modal/index.ts
+++ src/app/components/Modal/index.ts
@@ -1,9 +1,7 @@
 import ModalContent from "./ModalContent";
 import ModalRoot from "./ModalRoot";
-import ModalTrigger from "./ModalTrigger";
 
 export const Modal = {
   Root: ModalRoot,
-  Trigger: ModalTrigger,
   Content: ModalContent,
 };
